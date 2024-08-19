1663fde84b3d1d3af568e3a5487533b5c3482409
Hamza Yıldız <hamzayildiz2ekim@gmail.com>
2024-08-19 13:47:21 +0300

    SERVER JS - GET METHOD SAMPLE

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get('/api/passwords', (req, res)=>{
+
+    res.json({message:"Get all passwords"})
+});
+
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
 });
