a40a4803b59040500982f5585086bf5e6030d66d
Hamza Yıldız <hamzayildiz2ekim@gmail.com>
2024-08-21 16:12:12 +0300

    ERROR HANDLER
    using in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+
 
 connectDb();
 const app = express();
@@ -9,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json()); //response values are json format 
 app.use("/api/passwords", require("./routes/passwordRoutes"));
+app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
