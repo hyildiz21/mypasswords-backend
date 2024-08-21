894e3507cd69bee9959fa1593c040812b4a84657
Hamza Yıldız <hamzayildiz2ekim@gmail.com>
2024-08-21 16:03:48 +0300

    CRUD
    await
    asynchandler
    findById
    findByİdAndDelete-Update

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -13,30 +13,68 @@ const getPasswords = asyncHandler(async (req, res) => {
 //@desc create password
 //@route POST /api/passwords
 //@access public
-const createPassword = (req, res) => {
-    res.status(200).json({message:"create password"});
-};
+const createPassword = asyncHandler(async (req, res) => {
+    console.log("request body: ",req.body);
+    const {username, password} = req.body;
+    if (!username || !password) {
+        res.status(400)
+        throw new Error("All fields are mandatory!");
+    }
+    const passwordData = await Password.create({
+        username,
+        password,
+
+    });
+
+    res.status(201).json(passwordData);
+});
 
 //@desc Get password with id
 //@route GET /api/passwords/:id
 //@access public
-const getPassword = (req, res) => {
-    res.status(200).json({message:`get password for ${req.params.id}`});
-};
+const getPassword = asyncHandler(async (req, res) => {
+    const passwordData = await Password.findById(req.params.id);
+    if (!passwordData) {
+        res.status(404)
+        throw new Error("Password not found");
+    }
+    
+    res.status(200).json(passwordData);
+});
 
 //@desc Update password 
 //@route PUT /api/passwords
 //@access public
-const updatePassword = (req, res) => {
-    res.status(200).json({message:`update password for ${req.params.id}`});
-};
+const updatePassword =asyncHandler(async (req, res) => {
+    const passwordData = await Password.findById(req.params.id);
+
+    if (!passwordData) {
+        res.status(404)
+        throw new Error("Password not found");
+    }
+
+    const updatedPassword = await Password.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );   
+    res.status(200).json(updatedPassword);
+});
 
 //@desc Delete password 
 //@route DELETE /api/passwords
 //@access public
-const deletePassword = (req, res) => {
-    res.status(200).json({message:`delete password for ${req.params.id}`});
-};
+const deletePassword =asyncHandler(async (req, res) => {
+    const passwordData = await Password.findById(req.params.id);
+    if (!passwordData) {
+        res.status(404)
+        throw new Error("Password not found");
+    }
+
+    const deletedPassword = await Password.findByIdAndDelete(req.params.id);
+    
+    res.status(200).json(deletedPassword);
+});
 
 
 
